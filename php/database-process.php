2fa746c7998925bfaf79b6c369f4d7a1815e19aa
ElMachilow <U202114983@upc.edu.pe>
2024-05-20 08:06:29 -0500

    cambios

--- php/database-process.php
+++ php/database-process.php
@@ -3,7 +3,7 @@ $servername = "127.0.0.1:3308";
 $username = "root";
 $password = "mysql";
 $database = "alfa";
-
+ 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $database);
 
